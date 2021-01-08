a99db504ff2336be2a6843a4e918230e8d26c016
thekogo <thekogo@gmail.com>
2021-01-09 04:12:11 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<style>
+    @font-face {
+        font-family: Anakotmai;
+        src: url("{{ asset('fonts/anakotmai-medium.woff') }}");
+        font-weight: 500;
+    }
+
+    @font-face {
+        font-family: Anakotmai;
+        src: url("{{ asset('fonts/anakotmai-bold.woff') }}");
+        font-weight: bold;
+    }
+
+    @font-face {
+        font-family: Anakotmai;
+        src: url("{{ asset('fonts/anakotmai-light.woff') }}");
+        font-weight: lighter;
+    }
+
+    body {
+        font-family: Anakotmai;
+    }
+</style>
+<body>
+<body class="h-screen bg-white">
+  <div class="container mx-auto h-full">
+    <div class="w-32 mx-auto pt-10">
+      <img src="{{ asset('img/logo.png') }}" alt="logo">
+    </div>
+    <hr class="mt-10"/>
+    <nav class="flex justify-center gap-2 my-1 font-medium">
+      <a href="#" class="px-3 py-4 border-b-4 border-white hover-border-orange">
+        หน้าแรก
+      </a>
+      <a href="#" class="px-3 py-4 border-b-4 border-white hover-border-orange">
+        ณัฐชา
+      </a>
+      <a href="#" class="px-3 py-4 border-b-4 border-white hover-border-orange">
+        ทีมงาน
+      </a>
+      <a href="#" class="px-3 py-4 border-b-4 border-white hover-border-orange">
+        แกลลอรี่
+      </a>
+    </nav>
+    @yield('content')
+  </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="grid grid-row-2 grid-cols-12 gap-4">
+    <div class="col-start-2 col-span-7 row-span-2 shadow-xl" style="height: 35rem;">
+    <div class="draft h-full">
+        
+    </div>
+    </div>
+    <div class="col-span-3 row-span-1 shadow-xl">
+    <div class="draft h-full">
+        
+    </div>
+    </div>
+    <div class="col-span-3 row-span-1 shadow-xl">
+    <div class="draft h-full">
+        
+    </div>
+    </div>
+</div>
+<div class="grid grid-cols-12">
+    <div class="col-start-2 col-span-8"> 
+    <h2 class="text-3xl mt-10 mb-5 ml-10 font-medium underline">ข่าวสาร</h2>
+    <div class="grid grid-cols-8 pb-10"> <!-- news -->
+        <div class="col-span-3 draft h-72">
+        
+        </div>
+        <div class="col-span-5 py-2 px-7 relative flex flex-col justify-between">
+        <div>
+            <h4 class="text-2xl font-medium">Test Title ทดสอบ</h4>
+            <p class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, reprehenderit. Ratione facere dolorum, ipsam perferendis incidunt ad earum officia animi perspiciatis cupiditate at rem sequi praesentium non quibusdam delectus laudantium.</p>
+        </div>
+        <div>
+            <button class="border py-2 block w-32 ml-auto">
+            อ่านต่อ
+            </button>
+        </div>
+        </div>
+    </div> <!-- end news -->
+    <div class="grid grid-cols-8 pb-10"> <!-- news -->
+        <div class="col-span-3 draft h-72">
+        
+        </div>
+        <div class="col-span-5 py-2 px-7 relative flex flex-col justify-between">
+        <div>
+            <h4 class="text-2xl font-medium">Test Title ทดสอบ</h4>
+            <p class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, reprehenderit. Ratione facere dolorum, ipsam perferendis incidunt ad earum officia animi perspiciatis cupiditate at rem sequi praesentium non quibusdam delectus laudantium.</p>
+        </div>
+        <div>
+            <button class="border py-2 block w-32 ml-auto">
+            อ่านต่อ
+            </button>
+        </div>
+        </div>
+    </div> <!-- end news -->
+    <div class="grid grid-cols-8 pb-10"> <!-- news -->
+        <div class="col-span-3 draft h-72">
+        
+        </div>
+        <div class="col-span-5 py-2 px-7 relative flex flex-col justify-between">
+        <div>
+            <h4 class="text-2xl font-medium">Test Title ทดสอบ</h4>
+            <p class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, reprehenderit. Ratione facere dolorum, ipsam perferendis incidunt ad earum officia animi perspiciatis cupiditate at rem sequi praesentium non quibusdam delectus laudantium.</p>
+        </div>
+        <div>
+            <button class="border py-2 block w-32 ml-auto">
+            อ่านต่อ
+            </button>
+        </div>
+        </div>
+    </div> <!-- end news -->
+    <div class="grid grid-cols-8 pb-10"> <!-- news -->
+        <div class="col-span-3 draft h-72">
+        
+        </div>
+        <div class="col-span-5 py-2 px-7 relative flex flex-col justify-between">
+        <div>
+            <h4 class="text-2xl font-medium">Test Title ทดสอบ</h4>
+            <p class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, reprehenderit. Ratione facere dolorum, ipsam perferendis incidunt ad earum officia animi perspiciatis cupiditate at rem sequi praesentium non quibusdam delectus laudantium.</p>
+        </div>
+        <div>
+            <button class="border py-2 block w-32 ml-auto">
+            อ่านต่อ
+            </button>
+        </div>
+        </div>
+    </div> <!-- end news -->
+    </div> 
+    <div class="col-span-3">
+    <h2 class="text-3xl mt-10 font-medium underline">ช่องทางการติดตาม</h2>
+    </div>
+</div>
+@endsection
